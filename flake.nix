d4a541fa3affcac3c0e4736762e9f5d9198b3fa3
upiscium <130824002+upiscium@users.noreply.github.com>
2026-08-08 17:29:53 +0900

    Publish C++ CMake Agent template

--- flake.nix
+++ flake.nix
@@ -15,6 +15,10 @@
         path = ./templates/agent-base;
         description = "Generated Agent-ready base repository scaffold";
       };
+      agent-cpp-cmake = {
+        path = ./templates/agent-cpp-cmake;
+        description = "Generated Agent-ready C++/CMake repository scaffold";
+      };
       agent-nix = {
         path = ./templates/agent-nix;
         description = "Generated Agent-ready Nix flake repository scaffold";
